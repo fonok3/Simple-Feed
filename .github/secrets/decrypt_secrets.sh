a70b3f0ea39f557d5ea81a3bf3fa8ca3d9d9a037
Florian Herzog <fl.herzog@icloud.com>
2020-06-03 00:13:14 +0200

    #15 💚 Fix Bugs

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS_PROVISIONING" --output ./.github/secrets/Simple_Feed.mobileprovision ./.github/secrets/Simple_Feed.mobileprovision.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS_CERTIFICATE" --output ./.github/secrets/Simple_Feed_Distribution.cer ./.github/secrets/Simple_Feed_Distribution.cer.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS_CERTIFICATE" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
@@ -10,7 +10,7 @@ cp ./.github/secrets/Simple_Feed.mobileprovision ~/Library/MobileDevice/Provisio
 
 
 security create-keychain -p "" build.keychain
-security import ./.github/secrets/Simple_Feed_Distribution.cer -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
