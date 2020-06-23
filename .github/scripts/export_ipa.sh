4fdddbcb0697a092ff195f02239e78acfe94b895
Florian Herzog <fl.herzog@icloud.com>
2020-06-23 10:46:29 +0200

    #15 💚 Readd command

--- .github/scripts/export_ipa.sh
+++ .github/scripts/export_ipa.sh
@@ -3,6 +3,7 @@
 set -eo pipefail
 
 xcodebuild -archivePath $PWD/build/SimpleFeed.xcarchive \
+            -exportOptionsPlist $PWD/build/SimpleFeed.xcarchive/info.plist \
             -exportPath $PWD/build \
             -allowProvisioningUpdates \
             -exportArchive | xcpretty
