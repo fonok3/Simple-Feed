10baad29bf7d82b2a789293132470263a49d4ddb
Florian Herzog <fl.herzog@icloud.com>
2020-06-23 10:33:52 +0200

    #15 💚 New Xcode build command

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -2,15 +2,17 @@
 
 set -eo pipefail
 
-xcodebuild -resolvePackageDependencies -workspace ./Simple\ Feed.xcworkspace -scheme Simple\ Feed
+xcodebuild  -workspace Simple\ Feed.xcworkspace -scheme Simple\ Feed -archivePath $PWD/build/SimpleFeed.xcarchive -sdk iphoneos -configuration Release archive
 
-xcodebuild  -showBuildSettings \
-            -workspace Simple\ Feed.xcworkspace \
-            -scheme Simple\ Feed \
-
-xcodebuild  -workspace Simple\ Feed.xcworkspace \
-            -scheme Simple\ Feed \
-            -destination 'gerneric/platform=iOS' \
-            -configuration AppStoreDistribution \
-            -archivePath $PWD/build/SimpleFeed.xcarchive \
-            clean archive | xcpretty
+#xcodebuild -resolvePackageDependencies -workspace ./Simple\ Feed.xcworkspace -scheme Simple\ Feed
+#
+#xcodebuild  -showBuildSettings
+#            -workspace Simple\ Feed.xcworkspace \
+#            -scheme Simple\ Feed \
+#
+#xcodebuild -workspace Simple\ Feed.xcworkspace \
+#            -scheme Simple\ Feed \
+#            -sdk iphoneos \
+#            -configuration AppStoreDistribution \
+#            -archivePath $PWD/build/SimpleFeed.xcarchive \
+#            clean archive | xcpretty
