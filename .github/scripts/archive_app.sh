537e7749f6fe504b25d6070407867d2093251623
Florian Herzog <fl.herzog@icloud.com>
2020-06-23 10:39:50 +0200

    #15 🔥 Deleted unnecessary code

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -3,16 +3,3 @@
 set -eo pipefail
 
 xcodebuild  -workspace Simple\ Feed.xcworkspace -scheme Simple\ Feed -archivePath $PWD/build/SimpleFeed.xcarchive -sdk iphoneos -configuration Release archive
-
-#xcodebuild -resolvePackageDependencies -workspace ./Simple\ Feed.xcworkspace -scheme Simple\ Feed
-#
-#xcodebuild  -showBuildSettings
-#            -workspace Simple\ Feed.xcworkspace \
-#            -scheme Simple\ Feed \
-#
-#xcodebuild -workspace Simple\ Feed.xcworkspace \
-#            -scheme Simple\ Feed \
-#            -sdk iphoneos \
-#            -configuration AppStoreDistribution \
-#            -archivePath $PWD/build/SimpleFeed.xcarchive \
-#            clean archive | xcpretty
