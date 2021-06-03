2722c240d601f060276c41d984c08b19ec52b3b2
Florian Herzog <fl.herzog@icloud.com>
2021-06-03 20:35:47 +0200

    #27 🐛 Fix build

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -3,7 +3,7 @@
 
 set -eo pipefail
 
-xcodebuild -workspace Simple\ Feed.xcworkspace \
+xcodebuild -workspace Simple\ Feed.xcodeproj \
             -scheme Simple\ Feed \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
